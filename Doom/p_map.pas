9b3ac369f1c720a91bd5c33cede596b3f6c28a96
jval1972 <jvalavanis@gmail.com>
2020-03-08 17:16:36 +0200

    Fix SKULL attack when PASSOBJ flag is set
    Lost soul must does not slam into medikits etc when the PASSOBJ  is set

--- Doom/p_map.pas
+++ Doom/p_map.pas
@@ -452,6 +452,45 @@ begin
 end;
 
 //
+// JVAL: 20200308 - New function
+// P_ThingsInSameZ
+//
+function P_ThingsInSameZ(const A, B: Pmobj_t): boolean;
+var
+  Az1, Az2, Bz1, Bz2: fixed_t;
+begin
+  Az1 := A.z - A.height div 2;
+  if Az1 < A.floorz then
+    Az1 := A.floorz;
+  Az2 := Az1 + A.height;
+  if Az2 > A.ceilingz then
+  begin
+    Az2 := A.ceilingz;
+    Az1 := Az2 - A.height;
+    if Az1 < A.floorz then
+      Az1 := A.floorz;
+  end;
+
+  Bz1 := B.z - B.height div 2;
+  if Bz1 < B.floorz then
+    Bz1 := B.floorz;
+  Bz2 := Bz1 + B.height;
+  if Bz2 > B.ceilingz then
+  begin
+    Bz2 := B.ceilingz;
+    Bz1 := Bz2 - B.height;
+    if Bz1 < B.floorz then
+      Bz1 := B.floorz;
+  end;
+
+  result :=
+    IsIntegerInRange(Az1, Bz1, Bz2) or
+    IsIntegerInRange(Az2, Bz1, Bz2) or
+    IsIntegerInRange(Bz1, Az1, Az2) or
+    IsIntegerInRange(Bz2, Az1, Az2);
+end;
+
+//
 // PIT_CheckThing
 //
 function PIT_CheckThing(thing: Pmobj_t): boolean;
@@ -460,6 +499,7 @@ var
   solid: boolean;
   damage: integer;
   pushfactor: fixed_t;
+  doslam: boolean;
 begin
   if (thing.flags and (MF_SOLID or MF_SPECIAL or MF_SHOOTABLE)) = 0 then
   begin
@@ -552,18 +592,29 @@ begin
   // check for skulls slamming into things
   if tmthing.flags and MF_SKULLFLY <> 0 then
   begin
-    damage := ((P_Random mod 8) + 1) * tmthing.info.damage;
-    P_DamageMobj(thing, tmthing, tmthing, damage);
+    if (tmthing.flags2_ex and MF2_EX_PASSMOBJ <> 0) and (G_PlayingEngineVersion >=  VERSION205) then
+    // If the MF2_EX_PASSMOBJ flag is set:
+    //  -Skulls do no slam to pickable objects if the
+    //  -Skulls must be in same "Z"
+      doslam := (thing.flags and MF_SPECIAL = 0) and P_ThingsInSameZ(tmthing, thing)
+    else
+      doslam := true;
 
-    tmthing.flags := tmthing.flags and (not MF_SKULLFLY);
-    tmthing.momx := 0;
-    tmthing.momy := 0;
-    tmthing.momz := 0;
+    if doslam then
+    begin
+      damage := ((P_Random mod 8) + 1) * tmthing.info.damage;
+      P_DamageMobj(thing, tmthing, tmthing, damage);
 
-    P_SetMobjState(tmthing, statenum_t(tmthing.info.spawnstate));
+      tmthing.flags := tmthing.flags and (not MF_SKULLFLY);
+      tmthing.momx := 0;
+      tmthing.momy := 0;
+      tmthing.momz := 0;
 
-    result := false;  // stop moving
-    exit;
+      P_SetMobjState(tmthing, statenum_t(tmthing.info.spawnstate));
+
+      result := false;  // stop moving
+      exit;
+    end;
   end;
 
   // missiles can hit other things
