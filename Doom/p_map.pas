0cc41143f6c5d6f30df58302de84603c27bfdb23
jval1972 <jvalavanis@gmail.com>
2021-01-04 12:53:55 +0200

    Source code formatting

--- Doom/p_map.pas
+++ Doom/p_map.pas
@@ -3,7 +3,7 @@
 //  DelphiDoom: A modified and improved DOOM engine for Windows
 //  based on original Linux Doom as published by "id Software"
 //  Copyright (C) 1993-1996 by id Software, Inc.
-//  Copyright (C) 2004-2020 by Jim Valavanis
+//  Copyright (C) 2004-2021 by Jim Valavanis
 //
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
@@ -2308,7 +2308,7 @@ begin
   begin
     P_SetMobjState(thing, S_GIBS);
 
-    thing.flags := thing.flags and (not MF_SOLID);
+    thing.flags := thing.flags and not MF_SOLID;
     thing.height := 0;
     thing.radius := 0;
 
@@ -2336,7 +2336,7 @@ begin
 
   nofit := true;
 
-  if crushchange and ((leveltime and 3) = 0) then
+  if crushchange and (leveltime and 3 = 0) then
   begin
     P_DamageMobj(thing, nil, nil, 10);
 
