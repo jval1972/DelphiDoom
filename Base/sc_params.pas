6a1c3977f7fddf05384bae89c9a806858c04c7a0
jval1972 <jvalavanis@gmail.com>
2021-01-06 07:22:18 +0200

    Extra check for bool variables in state parameters

--- Base/sc_params.pas
+++ Base/sc_params.pas
@@ -3,7 +3,7 @@
 //  DelphiDoom: A modified and improved DOOM engine for Windows
 //  based on original Linux Doom as published by "id Software"
 //  Copyright (C) 1993-1996 by id Software, Inc.
-//  Copyright (C) 2004-2020 by Jim Valavanis
+//  Copyright (C) 2004-2021 by Jim Valavanis
 //
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
@@ -573,8 +573,13 @@ function TCustomParamList.GetBool(index: integer): boolean;
 var
   ret: string;
 begin
-  ret := SC_EvaluateActorExpression(fActor, fList[index].s_param);
-  result := ret = 'TRUE';
+  if (index >= 0) and (index < fNumItems) then
+  begin
+    ret := SC_EvaluateActorExpression(fActor, fList[index].s_param)
+    result := ret = 'TRUE';
+  end
+  else
+    result := false;
 end;
 
 function TCustomParamList.GetString(index: integer): string;
