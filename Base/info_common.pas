af0d28fda91043721ee72d0baf04fed51204eac9
jval1972 <jvalavanis@gmail.com>
2021-01-07 18:49:31 +0200

    Default values for scale, gravity and pushfactor for new mobjs

--- Base/info_common.pas
+++ Base/info_common.pas
@@ -3,7 +3,7 @@
 //  DelphiDoom: A modified and improved DOOM engine for Windows
 //  based on original Linux Doom as published by "id Software"
 //  Copyright (C) 1993-1996 by id Software, Inc.
-//  Copyright (C) 2004-2020 by Jim Valavanis
+//  Copyright (C) 2004-2021 by Jim Valavanis
 //
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
@@ -251,6 +251,9 @@ begin
   {$ENDIF}
   mobjinfo[nummobjtypes].inheritsfrom := -1; // Set to -1
   mobjinfo[nummobjtypes].doomednum := -1; // Set to -1
+  mobjinfo[nummobjtypes].pushfactor := DEFPUSHFACTOR;
+  mobjinfo[nummobjtypes].scale := FRACUNIT;
+  mobjinfo[nummobjtypes].gravity := FRACUNIT;
   result := nummobjtypes;
   inc(nummobjtypes);
 end;
