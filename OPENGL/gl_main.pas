de874d2e123ebaeeb06416b4bcb472971d90abdf
jval1972 <jvalavanis@gmail.com>
2021-01-09 15:37:35 +0200

    No delay to return to desktop when finished

--- OPENGL/gl_main.pas
+++ OPENGL/gl_main.pas
@@ -3,7 +3,7 @@
 //  DelphiDoom: A modified and improved DOOM engine for Windows
 //  based on original Linux Doom as published by "id Software"
 //  Copyright (C) 1993-1996 by id Software, Inc.
-//  Copyright (C) 2004-2020 by Jim Valavanis
+//  Copyright (C) 2004-2021 by Jim Valavanis
 //
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
@@ -728,6 +728,7 @@ begin
       WM_DESTROY:
         begin
           result := 0;
+          ShowWindow(hWnd, SW_HIDE);
           I_Destroy(0);
           exit;
         end;
