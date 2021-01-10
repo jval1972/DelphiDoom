56e55a48874d11e0a54d00c8e08b51d1ffc3c63a
jval1972 <jvalavanis@gmail.com>
2021-01-10 08:13:38 +0200

    Avoid crash in OpenGL mode if sprite frames are missing

--- OPENGL/gl_tex.pas
+++ OPENGL/gl_tex.pas
@@ -3,7 +3,7 @@
 //  DelphiDoom: A modified and improved DOOM engine for Windows
 //  based on original Linux Doom as published by "id Software"
 //  Copyright (C) 1993-1996 by id Software, Inc.
-//  Copyright (C) 2004-2020 by Jim Valavanis
+//  Copyright (C) 2004-2021 by Jim Valavanis
 //
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
@@ -1556,9 +1556,12 @@ begin
     begin
       for j := sprites[i].numframes - 1 downto 0 do
       begin
-        sflump := @sprites[i].spriteframes[j].lump;
-        for k := 7 downto 0 do
-          gld_BindPatch(gld_RegisterPatch(firstspritelump + sflump[k], Ord(CR_DEFAULT)), Ord(CR_DEFAULT));
+        if sprites[i].spriteframes[j].rotate >= 0 then
+        begin
+          sflump := @sprites[i].spriteframes[j].lump;
+          for k := 7 downto 0 do
+            gld_BindPatch(gld_RegisterPatch(firstspritelump + sflump[k], Ord(CR_DEFAULT)), Ord(CR_DEFAULT));
+        end;
       end;
     end;
 
