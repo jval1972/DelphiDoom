b60dd0955459d81eed39ef0429b099932b96de58
jval1972 <jvalavanis@gmail.com>
2021-01-16 10:28:21 +0200

    mystrcpy() function

---
+++ C_LIB/src/mystrcpy.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "mystrcpy.h"
+
+char* mystrcpy(char* dest, const char* src)
+{
+    char *ptr;
+    // return if no memory is allocated to the dest
+    if (!dest)
+        return NULL;
+
+    // make ptr pointing to the beginning of dest string
+    ptr = dest;
+
+    // copy the C-string pointed by src into the array
+    // pointed by dest
+    while (*src != '\0')
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+ 
+    // include the terminating null character
+    *dest = '\0';
+ 
+    // dest is returned by standard strcpy()
+    return ptr;
+}
---
+++ C_LIB/src/mystrcpy.h
@@ -0,0 +1,4 @@
+#ifndef _MYSTRCPY_H_
+#define _MYSTRCPY_H_
+char* mystrcpy(char* dest, const char* src);
+#endif
