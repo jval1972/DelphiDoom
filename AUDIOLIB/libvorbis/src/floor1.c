7bc73eab3b8ca6ce7421735f425921e4ac859c50
jval1972 <jvalavanis@gmail.com>
2021-01-16 13:35:52 +0200

    exit() in floor1.c replaced with (return NULL)

--- AUDIOLIB/libvorbis/src/floor1.c
+++ AUDIOLIB/libvorbis/src/floor1.c
@@ -643,7 +643,8 @@ int *floor1_fit(vorbis_block *vb,vorbis_look_floor1 *look,
           int hy=post_Y(fit_valueA,fit_valueB,hn);
 
           if(ly==-1 || hy==-1){
-            exit(1);
+            //exit(1);
+            return NULL;
           }
 
           if(inspect_error(lx,hx,ly,hy,logmask,logmdct,info)){
