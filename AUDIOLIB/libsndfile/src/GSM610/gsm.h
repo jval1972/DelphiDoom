a31d6abac70ee51a25c04b5f808609dbbfe59f57
jval1972 <jvalavanis@gmail.com>
2021-01-17 02:21:10 +0200

    Update gsm.h

--- AUDIOLIB/libsndfile/src/GSM610/gsm.h
+++ AUDIOLIB/libsndfile/src/GSM610/gsm.h
@@ -14,15 +14,15 @@
  */
 
 typedef struct gsm_state * 	gsm ;
-typedef short				gsm_signal ;		/* signed 16 bit */
+typedef short		    	gsm_signal ;		/* signed 16 bit */
 typedef unsigned char		gsm_byte ;
-typedef gsm_byte 			gsm_frame [33] ;	/* 33 * 8 bits	 */
+typedef gsm_byte 	    	gsm_frame [33] ;	/* 33 * 8 bits	 */
 
-#define GSM_MAGIC			0xD			/* 13 kbit/s RPE-LTP */
+#define GSM_MAGIC	    	0xD			/* 13 kbit/s RPE-LTP */
 
 #define	GSM_PATCHLEVEL		10
-#define	GSM_MINOR			0
-#define	GSM_MAJOR			1
+#define	GSM_MINOR	    	0
+#define	GSM_MAJOR	    	1
 
 #define	GSM_OPT_VERBOSE		1
 #define	GSM_OPT_FAST		2
@@ -48,5 +48,4 @@ int gsm_explode (gsm, gsm_byte *, gsm_signal *) ;
 void gsm_implode (gsm, gsm_signal *, gsm_byte *) ;
 
 #endif	/* GSM_H */
-
 
