19ab06939eb0354a07be24e0188a1ad9a161ab23
jval1972 <jvalavanis@gmail.com>
2021-01-17 02:29:03 +0200

    Use <stdlib.h> in os.h instead of <alloc.h>

--- AUDIOLIB/libvorbis/src/os.h
+++ AUDIOLIB/libvorbis/src/os.h
@@ -44,8 +44,9 @@
 #  define M_PI (3.1415926536f)
 #endif
 
+#include <stdlib.h>
 #if defined(_WIN32) && !defined(__SYMBIAN32__)
-#  include <alloc.h>
+//#  include <alloc.h>
 #  define rint(x)   (floor((x)+0.5f))
 #  define NO_FLOAT_MATH_LIB
 #  define FAST_HYPOT(a, b) sqrt((a)*(a) + (b)*(b))
