c325e7e23ed1f33331cb5b5710e491ee62715180
jval1972 <jvalavanis@gmail.com>
2021-01-22 07:09:06 +0200

    Accept next version config file

--- Base/m_misc.pas
+++ Base/m_misc.pas
@@ -3,7 +3,7 @@
 //  DelphiDoom: A modified and improved DOOM engine for Windows
 //  based on original Linux Doom as published by "id Software"
 //  Copyright (C) 1993-1996 by id Software, Inc.
-//  Copyright (C) 2004-2020 by Jim Valavanis
+//  Copyright (C) 2004-2021 by Jim Valavanis
 //
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
@@ -799,7 +799,7 @@ begin
     if s.Count > 1 then
     begin
       acceptablever := False;
-      for ver := VERSION downto 110 do
+      for ver := VERSION + 1 downto 110 do  // JVAL: 20210122 - Accept next version config
       begin
         sprintf(verstr, VERFMT, [ver div 100, 2, ver mod 100]);
         if Pos(verstr, s[0]) > 0 then
