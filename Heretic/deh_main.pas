9bb63926dc34b2a0a66ea1c64f58d40e029cf1b5
jval1972 <jvalavanis@gmail.com>
2021-02-11 16:37:30 +0200

    Added A_RearrangePointers() ACTORDEF function (Doom branch)

--- Heretic/deh_main.pas
+++ Heretic/deh_main.pas
@@ -48,7 +48,7 @@ procedure DEH_Init;
 procedure DEH_ShutDown;
 
 const
-  DEHNUMACTIONS = 351;
+  DEHNUMACTIONS = 352;
 
 type
   deh_action_t = record
@@ -3066,6 +3066,9 @@ begin
   deh_actions[350].action.acp1 := @A_MonsterRefire;
   deh_actions[350].name := strupper('MonsterRefire');
   {$IFDEF DLL}deh_actions[350].decl := 'A_MonsterRefire(prob: integer, offset: state_t)';{$ENDIF}
+  deh_actions[351].action.acp1 := @A_RearrangePointers;
+  deh_actions[351].name := strupper('RearrangePointers');
+  {$IFDEF DLL}deh_actions[351].decl := 'A_RearrangePointers(ptr_target: integer, ptr_master: integer, ptr_tracer: integer, flags: integer)';{$ENDIF}
 
   for i := 0 to DEHNUMACTIONS - 1 do
     DEH_AddActionToHash(deh_actions[i].name, i);
