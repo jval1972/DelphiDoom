76c9d55d8fafc3195e887fadf947bd713ab792cf
jval1972 <jvalavanis@gmail.com>
2021-02-11 17:28:09 +0200

    Added A_AlertMonsters() ACTORDEF function (Heretic branch)

--- Heretic/deh_main.pas
+++ Heretic/deh_main.pas
@@ -48,7 +48,7 @@ procedure DEH_Init;
 procedure DEH_ShutDown;
 
 const
-  DEHNUMACTIONS = 353;
+  DEHNUMACTIONS = 354;
 
 type
   deh_action_t = record
@@ -3072,6 +3072,9 @@ begin
   deh_actions[352].action.acp1 := @A_TransferPointer;
   deh_actions[352].name := strupper('TransferPointer');
   {$IFDEF DLL}deh_actions[352].decl := 'A_TransferPointer(ptr_source: integer, ptr_recipient: integer, ptr_sourcefield: integer, [ptr_recipientfield: integer], [flags: integer])';{$ENDIF}
+  deh_actions[353].action.acp1 := @A_AlertMonsters;
+  deh_actions[353].name := strupper('AlertMonsters');
+  {$IFDEF DLL}deh_actions[353].decl := 'A_AlertMonsters(maxdist: integer, flags: integer)';{$ENDIF}
 
   for i := 0 to DEHNUMACTIONS - 1 do
     DEH_AddActionToHash(deh_actions[i].name, i);
