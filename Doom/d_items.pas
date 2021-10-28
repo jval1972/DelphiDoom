c9f68693abf7a84e64f32b66ef8335e2eebae657
jval1972 <jvalavanis@gmail.com>
2021-10-28 14:31:15 +0300

    Source code formatting

--- Doom/d_items.pas
+++ Doom/d_items.pas
@@ -68,40 +68,40 @@ var
   weaponinfo: array[0..Ord(NUMWEAPONS) - 1] of weaponinfo_t = (
   // fist
     (ammo: am_noammo;            upstate: Ord(S_PUNCHUP);   downstate: Ord(S_PUNCHDOWN);
-     readystate: Ord(S_PUNCH);   atkstate: Ord(S_PUNCH1);   holdatkstate: 0;
+     readystate: Ord(S_PUNCH);   atkstate: Ord(S_PUNCH1);   holdatkstate: Ord(S_NULL);
      flashstate: Ord(S_NULL)),
   // pistol
     (ammo: am_clip;              upstate: Ord(S_PISTOLUP);  downstate: Ord(S_PISTOLDOWN);
-     readystate: Ord(S_PISTOL);  atkstate: Ord(S_PISTOL1);  holdatkstate: 0;
+     readystate: Ord(S_PISTOL);  atkstate: Ord(S_PISTOL1);  holdatkstate: Ord(S_NULL);
      flashstate: Ord(S_PISTOLFLASH)),
   // shotgun
     (ammo: am_shell;             upstate: Ord(S_SGUNUP);    downstate: Ord(S_SGUNDOWN);
-     readystate: Ord(S_SGUN);    atkstate: Ord(S_SGUN1);    holdatkstate: 0;
+     readystate: Ord(S_SGUN);    atkstate: Ord(S_SGUN1);    holdatkstate: Ord(S_NULL);
      flashstate: Ord(S_SGUNFLASH1)),
   // chaingun
     (ammo: am_clip;              upstate: Ord(S_CHAINUP);   downstate: Ord(S_CHAINDOWN);
-     readystate: Ord(S_CHAIN);   atkstate: Ord(S_CHAIN1);   holdatkstate: 0;
+     readystate: Ord(S_CHAIN);   atkstate: Ord(S_CHAIN1);   holdatkstate: Ord(S_NULL);
      flashstate: Ord(S_CHAINFLASH1)),
   // missile launcher
     (ammo: am_misl;              upstate: Ord(S_MISSILEUP); downstate: Ord(S_MISSILEDOWN);
-     readystate: Ord(S_MISSILE); atkstate: Ord(S_MISSILE1); holdatkstate: 0;
+     readystate: Ord(S_MISSILE); atkstate: Ord(S_MISSILE1); holdatkstate: Ord(S_NULL);
      flashstate: Ord(S_MISSILEFLASH1)),
   // plasma rifle
      (ammo: am_cell;             upstate: Ord(S_PLASMAUP);  downstate: Ord(S_PLASMADOWN);
-      readystate: Ord(S_PLASMA); atkstate: Ord(S_PLASMA1);  holdatkstate: 0;
-     flashstate: Ord(S_PLASMAFLASH1)),
+      readystate: Ord(S_PLASMA); atkstate: Ord(S_PLASMA1);  holdatkstate: Ord(S_NULL);
+      flashstate: Ord(S_PLASMAFLASH1)),
   // bfg 9000
      (ammo: am_cell;             upstate: Ord(S_BFGUP);     downstate: Ord(S_BFGDOWN);
-      readystate: Ord(S_BFG);    atkstate: Ord(S_BFG1);     holdatkstate: 0;
-     flashstate: Ord(S_BFGFLASH1)),
+      readystate: Ord(S_BFG);    atkstate: Ord(S_BFG1);     holdatkstate: Ord(S_NULL);
+      flashstate: Ord(S_BFGFLASH1)),
   // chainsaw
      (ammo: am_noammo;           upstate: Ord(S_SAWUP);     downstate: Ord(S_SAWDOWN);
-      readystate: Ord(S_SAW);    atkstate: Ord(S_SAW1);     holdatkstate: 0;
-     flashstate: Ord(S_NULL)),
+      readystate: Ord(S_SAW);    atkstate: Ord(S_SAW1);     holdatkstate: Ord(S_NULL);
+      flashstate: Ord(S_NULL)),
   // super shotgun
      (ammo: am_shell;            upstate: Ord(S_DSGUNUP);   downstate: Ord(S_DSGUNDOWN);
-      readystate: Ord(S_DSGUN);  atkstate: Ord(S_DSGUN1);   holdatkstate: 0;
-     flashstate: Ord(S_DSGUNFLASH1))
+      readystate: Ord(S_DSGUN);  atkstate: Ord(S_DSGUN1);   holdatkstate: Ord(S_NULL);
+      flashstate: Ord(S_DSGUNFLASH1))
   );
 
 implementation
