05ce0abea677522173df246b0d7f18ff8c4b682d
jval1972 <jvalavanis@gmail.com>
2021-10-29 16:27:38 +0300

    Fix to weapon parsing

--- Base/sc_actordef.pas
+++ Base/sc_actordef.pas
@@ -1601,7 +1601,7 @@ var
           end;
       end;
 
-      sss1 := 'S_WEAPON_' + itoa(wpn.weaponno) + '_' + sss;
+      sss1 := 'S_WEAPON' + itoa(wpn.weaponno) + '_' + sss;
       result := statenames.IndexOfToken(sss1);
     end;
 
