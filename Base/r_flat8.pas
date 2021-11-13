950c7e605b39fc1cdab783c742760d051843501a
jval1972 <jvalavanis@gmail.com>
2021-11-13 05:59:20 +0200

    Fixed again glitch in large flats multithreading software rendering
    8 bit color

--- Base/r_flat8.pas
+++ Base/r_flat8.pas
@@ -3,7 +3,7 @@
 //  DelphiDoom: A modified and improved DOOM engine for Windows
 //  based on original Linux Doom as published by "id Software"
 //  Copyright (C) 1993-1996 by id Software, Inc.
-//  Copyright (C) 2004-2020 by Jim Valavanis
+//  Copyright (C) 2004-2021 by Jim Valavanis
 //
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
@@ -926,6 +926,7 @@ var
   ds_xstep: fixed_t;
   ds_ystep: fixed_t;
   ds_scale: dsscale_t;
+  ds_size: integer;
   xfrac: fixed_t;
   yfrac: fixed_t;
   xstep: fixed_t;
@@ -966,6 +967,7 @@ var
   ds_xstep: fixed_t;
   ds_ystep: fixed_t;
   ds_scale: dsscale_t;
+  ds_size: integer;
   xfrac: fixed_t;
   yfrac: fixed_t;
   xstep: fixed_t;
