bbaf579db100fe7ba20a06ee82a5d230ffbf01f0
jval1972 <jvalavanis@gmail.com>
2021-11-17 20:42:29 +0200

    Reset extra player fields when spawning player (Heretic branch)

--- Heretic/p_mobj.pas
+++ Heretic/p_mobj.pas
@@ -1331,6 +1331,13 @@ begin
   p.extralight := 0;
   p.fixedcolormap := 0;
   p.viewheight := PVIEWHEIGHT;
+  // JVAL: 20211117 - Reset extra player fields when spawning player
+  p.laddertics := 0;
+  p.slopetics := 0;
+  p.teleporttics := 0;
+  p.nextoof := 0;
+  p.quakeintensity := 0;
+  p.quaketics := 0;
 
   // setup gun psprite
   P_SetupPsprites(p);
