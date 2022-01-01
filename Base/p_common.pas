b455d8e82fbbc4192cadc3e678239ec51731c0c6
jval1972 <jvalavanis@gmail.com>
2022-01-01 15:50:15 +0200

    Update A_RandomGoto() ACTORDEF function

--- Base/p_common.pas
+++ Base/p_common.pas
@@ -3605,8 +3605,12 @@ begin
   idx := N_Random mod actor.state.params.Count;
 
   if not actor.state.params.IsComputed[idx] then
-    actor.state.params.IntVal[idx] := P_GetStateFromName(actor, actor.state.params.StrVal[idx]);
-  newstate := actor.state.params.IntVal[idx];
+  begin
+    newstate := P_GetStateFromName(actor, actor.state.params.StrVal[idx]);
+    actor.state.params.IntVal[idx] := newstate;
+  end
+  else
+    newstate := actor.state.params.IntVal[idx];
 
   P_SetMobjState(actor, statenum_t(newstate));
 end;
