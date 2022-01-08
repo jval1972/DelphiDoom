293a0811fd6e91551c2927d3b08dd5b3a318777b
jval1972 <jvalavanis@gmail.com>
2022-01-08 18:05:45 +0200

    Corrected copyright notice in p_sounds.pas (Strife branch)

--- Strife/p_sounds.pas
+++ Strife/p_sounds.pas
@@ -1,8 +1,15 @@
 //------------------------------------------------------------------------------
 //
-//  DelphiDoom: A modified and improved DOOM engine for Windows
-//  based on original Linux Doom as published by "id Software"
+//  DelphiStrife: A modified and improved Strife source port for Windows.
+//
+//  Based on:
+//    - Linux Doom by "id Software"
+//    - Chocolate Strife by "Simon Howard"
+//    - DelphiDoom by "Jim Valavanis"
+//
 //  Copyright (C) 1993-1996 by id Software, Inc.
+//  Copyright (C) 2005 Simon Howard
+//  Copyright (C) 2010 James Haley, Samuel Villarreal
 //  Copyright (C) 2004-2022 by Jim Valavanis
 //
 //  This program is free software; you can redistribute it and/or
@@ -17,7 +24,7 @@
 //
 //  You should have received a copy of the GNU General Public License
 //  along with this program; if not, write to the Free Software
-//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+//  Foundation, inc., 59 Temple Place - Suite 330, Boston, MA
 //  02111-1307, USA.
 //
 //------------------------------------------------------------------------------
