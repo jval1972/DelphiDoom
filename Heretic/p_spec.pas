4107e5703ee3967dc6ee351f961e4ac15c14a749
jval1972 <jvalavanis@gmail.com>
2022-01-08 07:10:13 +0200

    Updated sourceforge site

--- Heretic/p_spec.pas
+++ Heretic/p_spec.pas
@@ -29,7 +29,7 @@
 //   Line Tag handling. Line and Sector triggers.
 //
 //------------------------------------------------------------------------------
-//  Site  : http://sourceforge.net/projects/delphidoom/
+//  Site  : https://sourceforge.net/projects/delphidoom/
 //------------------------------------------------------------------------------
 
 {$I Doom32.inc}
