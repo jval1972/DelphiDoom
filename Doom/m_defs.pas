2d04f7c1a01cf0ad3ffbf1491fd608c77245ecd1
jval1972 <jvalavanis@gmail.com>
2022-01-25 05:53:26 +0200

    The default value of autoloadgwafiles console variable changed to false

--- Doom/m_defs.pas
+++ Doom/m_defs.pas
@@ -818,7 +818,7 @@ const
      setable: DFS_ALWAYS;
      defaultsvalue: '';
      defaultivalue: 0;
-     defaultbvalue: true;
+     defaultbvalue: false;
      _type: tBoolean),
 
     (name: 'Voxels';
--- Heretic/m_defs.pas
+++ Heretic/m_defs.pas
@@ -776,7 +776,7 @@ const
      setable: DFS_ALWAYS;
      defaultsvalue: '';
      defaultivalue: 0;
-     defaultbvalue: true;
+     defaultbvalue: false;
      _type: tBoolean),
 
     (name: 'Voxels';
