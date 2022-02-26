392f886a8ec0e6131dfd622eb51bc9dee662431a
jval1972 <jvalavanis@gmail.com>
2022-02-26 17:50:50 +0200

    dded NOJUMP option in UMAPINFO (Strife branch)

--- Strife/p_user.pas
+++ Strife/p_user.pas
@@ -555,6 +555,8 @@ begin
   // JVAL: 20220225 - NOJUMP sector flag (UDMF)
   if Psubsector_t(player.mo.subsector).sector.flags and SF_NOJUMP <> 0 then
     cmd_jump := 0
+  else if (gamemapinfo <> nil) and gamemapinfo.nojump then
+    cmd_jump := 0
   else
     cmd_jump := (cmd.jump_crouch and CMD_JUMP_MASK) shr CMD_JUMP_SHIFT;
 
