995706758d90691e14fa78067483e4756e34f434
jval1972 <jvalavanis@gmail.com>
2022-03-01 15:35:22 +0200

    Small optimization to masked middle textute rendering.
    Software rendering

--- Base/r_segs.pas
+++ Base/r_segs.pas
@@ -313,6 +313,7 @@ var
   mc2height: integer;
   texturecolumn: integer;
   t: double;
+  texturemid_dbl: double;
 {$IFDEF DOOM_OR_STRIFE}
   tempsec: sector_t;
 {$ENDIF}
@@ -423,6 +424,7 @@ begin
     dc_texturemid := dc_texturemid - viewz;
   end;
   dc_texturemid := dc_texturemid + curline.sidedef.rowoffset + curline.sidedef.midrowoffset;
+  texturemid_dbl := dc_texturemid / FRACUNIT;
 
   if fixedcolormap <> nil then
     dc_colormap := fixedcolormap;
@@ -494,7 +496,7 @@ begin
       sprtopscreen := FixedInt64(t); }
 
      //t -> double (delphidoom)
-      t := (centeryfrac / FRACUNIT) - (dc_texturemid / FRACUNIT) * (spryscale / FRACUNIT);
+      t := centery - texturemid_dbl * (spryscale / FRACUNIT);
       if (t + (textureheight[texnum] / FRACUNIT) * (spryscale / FRACUNIT) < 0) or (t > SCREENHEIGHT * 2) or (t < -32000.0) then
       begin
         if ds.use_double then
