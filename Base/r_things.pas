8bc2abe596a1840e64e868ea4b2fb58795df7a62
jval1972 <jvalavanis@gmail.com>
2022-03-15 10:52:59 +0200

    Fixed typo in comment

--- Base/r_things.pas
+++ Base/r_things.pas
@@ -1335,7 +1335,7 @@ end;
 //==============================================================================
 //
 // R_DrawVisSpriteLight
-// Used for sprites that emits light
+// Used for sprites that emit light
 //
 //==============================================================================
 procedure R_DrawVisSpriteLight(vis: Pvissprite_t; x1: integer; x2: integer);
