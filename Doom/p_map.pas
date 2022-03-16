4d7d5c137e521c2f8bcd6939dd0d93eee63bc6cc
jval1972 <jvalavanis@gmail.com>
2022-03-16 18:43:10 +0200

    Fixed map collisions for PASSMOBJ

--- Doom/p_map.pas
+++ Doom/p_map.pas
@@ -727,20 +727,6 @@ end;
 
 //==============================================================================
 //
-// JVAL: 20220313 - New function
-// P_ThingHeightOffsZ
-//
-//==============================================================================
-function P_ThingHeightOffsZ(const mo: Pmobj_t): fixed_t;
-begin
-  if mo.flags and MF_SPAWNCEILING <> 0 then
-    result := mo.height
-  else
-    result := 0;
-end;
-
-//==============================================================================
-//
 // JVAL: 20200308 - New function
 // P_ThingsInSameZ206
 //
@@ -788,14 +774,19 @@ end;
 function P_ThingsInSameZ(const A, B: Pmobj_t): boolean;
 var
   Az1, Az2, Bz1, Bz2: fixed_t;
-begin
-  if G_PlayingEngineVersion < VERSION207 then
+
+  function _getheightoffs(const mo: Pmobj_t): fixed_t;
   begin
-    result := P_ThingsInSameZ206(A, B);
-    exit;
+    if (G_PlayingEngineVersion < VERSION207) or ((A.flags2_ex and MF2_EX_ONMOBJ = 0) and (B.flags2_ex and MF2_EX_ONMOBJ = 0)) then
+      result := mo.height div 2
+    else if mo.flags and MF_SPAWNCEILING <> 0 then
+      result := mo.height
+    else
+      result := 0;
   end;
 
-  Az1 := A.z - P_ThingHeightOffsZ(A);
+begin
+  Az1 := A.z - _getheightoffs(A);
   if Az1 < A.floorz then
     Az1 := A.floorz;
   Az2 := Az1 + A.height;
@@ -807,7 +798,7 @@ begin
       Az1 := A.floorz;
   end;
 
-  Bz1 := B.z - P_ThingHeightOffsZ(B);
+  Bz1 := B.z - _getheightoffs(B);
   if Bz1 < B.floorz then
     Bz1 := B.floorz;
   Bz2 := Bz1 + B.height;
@@ -819,11 +810,18 @@ begin
       Bz1 := B.floorz;
   end;
 
-  result :=
-    IsIntegerInRange(Az1, Bz1 + 1, Bz2 - 1) or
-    IsIntegerInRange(Az2, Bz1 + 1, Bz2 - 1) or
-    IsIntegerInRange(Bz1, Az1 + 1, Az2 - 1) or
-    IsIntegerInRange(Bz2, Az1 + 1, Az2 - 1);
+  if (G_PlayingEngineVersion < VERSION207) or ((A.flags2_ex and MF2_EX_ONMOBJ = 0) and (B.flags2_ex and MF2_EX_ONMOBJ = 0)) then
+    result :=
+      IsIntegerInRange(Az1, Bz1, Bz2) or
+      IsIntegerInRange(Az2, Bz1, Bz2) or
+      IsIntegerInRange(Bz1, Az1, Az2) or
+      IsIntegerInRange(Bz2, Az1, Az2)
+  else
+    result :=
+      IsIntegerInRange(Az1, Bz1 + 1, Bz2 - 1) or
+      IsIntegerInRange(Az2, Bz1 + 1, Bz2 - 1) or
+      IsIntegerInRange(Bz1, Az1 + 1, Az2 - 1) or
+      IsIntegerInRange(Bz2, Az1 + 1, Az2 - 1)
 end;
 
 type
