969b9e878c0566b0e9c2beb6c26cd56a1e90106b
jval1972 <jvalavanis@gmail.com>
2022-03-16 18:48:15 +0200

    Removed unused function

--- Doom/p_map.pas
+++ Doom/p_map.pas
@@ -727,47 +727,6 @@ end;
 
 //==============================================================================
 //
-// JVAL: 20200308 - New function
-// P_ThingsInSameZ206
-//
-//==============================================================================
-function P_ThingsInSameZ206(const A, B: Pmobj_t): boolean;
-var
-  Az1, Az2, Bz1, Bz2: fixed_t;
-begin
-  Az1 := A.z - A.height div 2;
-  if Az1 < A.floorz then
-    Az1 := A.floorz;
-  Az2 := Az1 + A.height;
-  if Az2 > A.ceilingz then
-  begin
-    Az2 := A.ceilingz;
-    Az1 := Az2 - A.height;
-    if Az1 < A.floorz then
-      Az1 := A.floorz;
-  end;
-
-  Bz1 := B.z - B.height div 2;
-  if Bz1 < B.floorz then
-    Bz1 := B.floorz;
-  Bz2 := Bz1 + B.height;
-  if Bz2 > B.ceilingz then
-  begin
-    Bz2 := B.ceilingz;
-    Bz1 := Bz2 - B.height;
-    if Bz1 < B.floorz then
-      Bz1 := B.floorz;
-  end;
-
-  result :=
-    IsIntegerInRange(Az1, Bz1, Bz2) or
-    IsIntegerInRange(Az2, Bz1, Bz2) or
-    IsIntegerInRange(Bz1, Az1, Az2) or
-    IsIntegerInRange(Bz2, Az1, Az2);
-end;
-
-//==============================================================================
-//
 // P_ThingsInSameZ
 //
 //==============================================================================
