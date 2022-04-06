6cbe3eb877fba4947dad689184dad4aa2254c900
jval1972 <jvalavanis@gmail.com>
2022-04-06 17:30:36 +0300

    "r_voxellightmapaccuracy" console variable can be set from the Lightmap menu (Hexen branch)

--- Hexen/m_menu.pas
+++ Hexen/m_menu.pas
@@ -294,6 +294,7 @@ uses
 {$IFNDEF OPENGL}
   r_fake3d,
   r_softlights,
+  r_voxels,
 {$ENDIF}
   r_camera,
   r_draw,
@@ -1198,6 +1199,7 @@ type
     ol_uselightmaps,
     ol_lightmaponmasked,
     ol_lightmaponemitters,
+    ol_lightmapvoxelaccuracy,
     ol_lightmapfunc,
     ol_colorintensity,
     ol_filler1,
@@ -3379,6 +3381,20 @@ end;
 
 //==============================================================================
 //
+// M_ChangeLightmapVoxelAccuracy
+//
+//==============================================================================
+procedure M_ChangeLightmapVoxelAccuracy(choice: integer);
+begin
+  r_voxellightmapaccuracy := r_voxellightmapaccuracy + 1;
+  if r_voxellightmapaccuracy < 0 then
+    r_voxellightmapaccuracy := 2
+  else if r_voxellightmapaccuracy > 2 then
+    r_voxellightmapaccuracy := 0;
+end;
+
+//==============================================================================
+//
 // M_LightmapDefaults
 //
 //==============================================================================
@@ -3387,9 +3403,14 @@ begin
   lightmapcolorintensity := DEFLMCOLORSENSITIVITY;
   lightwidthfactor := DEFLIGHTWIDTHFACTOR;
   r_lightmaponmasked := true;
+  r_voxellightmapaccuracy := 1;
   r_lightmaponemitters := false;
   r_lightmapfadeoutfunc := 0;
 end;
+
+const
+  strlightmapvoxelaccuracy: array[0..2] of string =
+    ('LOW', 'NORMAL', 'HIGH');
 
 const
   strlightmapfadefunc: array[0..NUMLIGHTMAPFADEOUTFUNCS - 1] of string =
@@ -3405,6 +3426,11 @@ var
   ppos: menupos_t;
 begin
   M_DrawDisplayOptions;
+
+  r_voxellightmapaccuracy := ibetween(r_voxellightmapaccuracy, 0, 2);
+
+  ppos := M_WriteText3(OptionsLightmapDef.x, OptionsLightmapDef.y + OptionsLightmapDef.itemheight * Ord(ol_lightmapvoxelaccuracy), 'Voxel illumination accuracy: ');
+  M_WriteWhiteText3(ppos.x, ppos.y, strlightmapvoxelaccuracy[r_voxellightmapaccuracy]);
 
   r_lightmapfadeoutfunc := ibetween(r_lightmapfadeoutfunc, 0, NUMLIGHTMAPFADEOUTFUNCS - 1);
   lightmapcolorintensityidx := (lightmapcolorintensity - 32) div 8;
@@ -6111,6 +6137,14 @@ begin
   pmi.routine := @M_BoolCmd;
   pmi.pBoolVal := @r_lightmaponemitters;
   pmi.alphaKey := 'e';
+
+  inc(pmi);
+  pmi.status := 1;
+  pmi.name := '!Voxel illumination accuracy';
+  pmi.cmd := '';
+  pmi.routine := @M_ChangeLightmapVoxelAccuracy;
+  pmi.pBoolVal := nil;
+  pmi.alphaKey := 'v';
 
   inc(pmi);
   pmi.status := 1;
