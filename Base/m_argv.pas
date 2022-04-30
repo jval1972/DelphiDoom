0972c83752832c6f901534432851011d304fc8b9
jval1972 <jvalavanis@gmail.com>
2022-04-30 13:00:26 +0300

    Fix problem when starting from different folder than the executable
    https://www.doomworld.com/forum/topic/92113-delphidoom-207734-udmf-umapinfo-mbf21-apr-28-2022/?do=findComment&comment=2487932

--- Base/m_argv.pas
+++ Base/m_argv.pas
@@ -238,6 +238,9 @@ begin
       myargc := i + 1;
       exit;
     end;
+    if myargv[i] <> '' then
+      if fexists(myargv[i]) then
+        myargv[i] := fexpand(myargv[i]);
   end;
 
   defargv := DEFARGVFILENAME;
@@ -257,6 +260,7 @@ begin
     cmdln := cmdln + ' ' + myargv[i];
   printf('%s'#13#10, [cmdln]);
 
+  ChDir(fpath(myargv[0]));
 end;
 
 //==============================================================================
